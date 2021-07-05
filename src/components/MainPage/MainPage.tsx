0bf0c0e5bf79a6d7f5cd71a2b20d5326451e207a
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-07-05 22:18:12 +0200

    add first component - MainPage with one header type h1 to tests

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,10 @@
+import React, {FC} from 'react';
+
+
+export const MainPage: FC = () => {
+    return (
+        <div>
+            <h1>test nowej strony w react - countries app</h1>
+        </div>
+    )
+}
