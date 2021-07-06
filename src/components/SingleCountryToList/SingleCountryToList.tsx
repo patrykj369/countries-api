fcb078d448962bb2cc1b8e3e9eacd885ab9f6d0e
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-07-06 22:05:37 +0200

    addded a few styledHelpers

--- src/components/SingleCountryToList/SingleCountryToList.tsx
+++ src/components/SingleCountryToList/SingleCountryToList.tsx
@@ -3,19 +3,14 @@ import styled from 'styled-components'
 
 import {device} from './../../styledHelpers/deviceWidth'
 
-const MyComponent = styled.div`
-    background-color: aqua;
-    min-height: 100vh;
-    margin: 0 auto;
-    @media ${device.mobileS}{
-        max-width: 320px;
-    }
+const SingleCountryToList = styled.div`
+
 `;
 
 export const MainPage: FC = () => {
     return (
-        <MyComponent>
+        <SingleCountryToList>
             <h1>test nowej strony w react - countries app</h1>
-        </MyComponent>
+        </SingleCountryToList>
     )
 }
---
+++ src/styledHelpers/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    darkBlueDM: "hsl(209, 23%, 22%)",
+    veryDarkBlueDM: "hsl(207, 26%, 17%)",
+    veryDarkBlueLM: "hsl(200, 15%, 8%)",
+    darkGrayLM: "hsl(0, 0%, 52%)",
+    veryLightGrayLM: "hsl(0, 0%, 98%)",
+    whiteDMLM: "hsl(0, 0%, 100%)"
+}
