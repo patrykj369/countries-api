b8ac70ac34699bb96bf5cf99439a1a4b476ceed8
Patryk Jabłoński <patryk.jablonski@microsoft.wsei.edu.pl>
2021-07-08 22:13:44 +0200

    finished searchbar

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -2,19 +2,35 @@ import React, {FC} from 'react';
 import styled from 'styled-components';
 
 import {device} from './../../styledHelpers/deviceWidth';
+import {colors} from './../../styledHelpers/colors';
 import { Navbar } from '../Navbar/Navbar';
+import { Searchbar} from '../Searchbar/Searchbar';
 
 const MyComponent = styled.div`
     min-height: 100vh;
     margin: 0 auto;
+    background-color: ${colors.veryLightGrayLM};
     @media ${device.mobileS}{
     }
+
+    .searchbar{
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        margin-top: 1.5rem;
+    }
 `;
+
 
 export const MainPage: FC = () => {
     return (
         <MyComponent>
             <Navbar/>
+
+            <div className="searchbar">
+                <Searchbar/>
+            </div>
+
         </MyComponent>
     )
 }
--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,8 +1,37 @@
 import React, {FC} from 'react';
 import styled from 'styled-components';
+import { fontSize } from '../../styledHelpers/fontSize';
+import {colors} from './../../styledHelpers/colors';
+
+const Wrapper = styled.div`
+    height: 8vh;
+    width: 90vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 .5rem;
+    border: none;
+    box-shadow: 0px 2px 7px -1px rgb(185, 185, 185);
+    background-color: ${colors.whiteDMLM};
+
+    input {
+        height: 7.8vh;
+        width: 70%;
+        font-size: ${fontSize[16]};
+        border: none;
+    }
+
+    img {
+        height: 1.3rem;
+        margin-right: .5rem;
+    }
+`;
 
 export const Searchbar: FC = () => {
     return(
-
+        <Wrapper>
+            <img src="./icons/search.svg" alt="searchbar"/>
+            <input type="text" placeholder="Search for a country..."></input>
+        </Wrapper>
     )
 }
